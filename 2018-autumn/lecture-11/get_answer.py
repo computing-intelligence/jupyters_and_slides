2deb1e58c5f8f005133cfb4530c2b673431b8bfe
fortymiles <mqgao@outlook.com>
2019-01-13 17:33:33 +0800

    add lecture 11 related code

---
+++ 2018-autumn/lecture-11/get_answer.py
@@ -0,0 +1,18 @@
+# Created by mqgao at 2019/1/13
+
+"""
+Feature: #Enter feature name here
+# Enter feature description here
+
+Scenario: #Enter scenario name here
+# Enter steps here
+
+Test File Location: # Enter
+
+"""
+
+
+def get_elephant_type(big_ear, long_teeth, larger_size):
+    if big_ear and long_teeth and larger_size: return 'male'
+    else:
+        return 'female'
